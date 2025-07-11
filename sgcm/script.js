2b1c26c9e1dd66c2d70ccadd72f5caf7860b885c
elyvanbs <elyvanbs@gmail.com>
2025-07-11 17:57:42 -0500

    fix: Faz com que o ID da table não se repita

--- sgcm/script.js
+++ sgcm/script.js
@@ -1,4 +1,9 @@
+let ultimoId = 0; // Controla o maior ID já usado
+
 function carregarProfissionais() {
+
+    
+
     let xhr = new XMLHttpRequest();
     xhr.open('GET', 'http://my-json-server.typicode.com/juniorlimeiras/json/profissionais');
     xhr.addEventListener('readystatechange', () => {
@@ -6,41 +11,20 @@ function carregarProfissionais() {
             let dados = JSON.parse(xhr.responseText);
 
             for (let item of dados) {
-                inserirProfissional(item);
-                // let linha = document.createElement('tr');
-                // let id = document.createElement('td');
-                // let nome = document.createElement('td');
-                // let registro = document.createElement('td');
-                // let telefone = document.createElement('td');
-                // let email = document.createElement('td');
-                // let unidade = document.createElement('td');
-                // let especialidade = document.createElement('td');
-                // let opcoes = document.createElement('td');
+    inserirProfissional(item);
+    if (item.id > ultimoId) {
+        ultimoId = item.id; // Atualiza o maior ID carregado
+    }
+}
 
-                // id.textContent = item.id;
-                // nome.textContent = item.nome;
-                // registro.textContent = item.registro;
-                // telefone.textContent = item.telefone;
-                // email.textContent = item.email;
-                // unidade.textContent = item.unidade;
-                // especialidade.textContent = item.especialidade;
-                // opcoes.innerHTML = `<a class="botao_verde" href="">Editar</a>|<a class="botao_vermelho" href="javascript:void(0)">Excluir</a>`;
-
-                // linha.appendChild(id);
-                // linha.appendChild(nome);
-                // linha.appendChild(registro);
-                // linha.appendChild(telefone);
-                // linha.appendChild(email);
-                // linha.appendChild(unidade);
-                // linha.appendChild(especialidade);
-                // linha.appendChild(opcoes);
-                // tabela.appendChild(linha);
-            }
         }
+        
         excluirLinha();
     });//Fecha o escutador de evento
     xhr.send();
+    
 }
+
 carregarProfissionais();
 
 let tabela = document.querySelector('table tbody');
@@ -51,7 +35,7 @@ btn_enviar.addEventListener('click', (event) => {
     event.preventDefault(); //Evita que a página seja recarregada
     //let objeto = new Object();
     let objeto = {
-        id: tabela.rows.length,
+        id: ++ultimoId,
         nome: form.nome.value,
         registro: form.registroConselho.value,
         telefone: form.telefone.value,
